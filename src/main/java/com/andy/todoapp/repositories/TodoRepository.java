cb0a2f8d097a243af3f60427c6cb6ec1dfaad4d2
andy <andrew.bandura@gmail.com>
2018-02-13 01:07:51 +0200

    -initial commit

---
+++ src/main/java/com/andy/todoapp/repositories/TodoRepository.java
@@ -0,0 +1,10 @@
+package com.andy.todoapp.repositories;
+
+import com.andy.todoapp.models.Todo;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TodoRepository extends MongoRepository<Todo, String> {
+
+}
